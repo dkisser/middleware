b3121c4bf77e27e86de7438b00761acef827104d
dkisser <437631891@qq.com>
2020-05-14 16:37:53 +0800

    修改启动接口配置

--- TinyCat/TinyCat-core/src/main/java/org/wc/webserver/spring/SpringApplicationContext.java
+++ TinyCat/TinyCat-core/src/main/java/org/wc/webserver/spring/SpringApplicationContext.java
@@ -1,22 +1,16 @@
 package org.wc.webserver.spring;
 
-import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
-import org.springframework.stereotype.Component;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
 
 /**
  * Created by WenChen on 2020/1/19.
  */
-@Component
-public class SpringApplicationContext implements ApplicationContextAware{
+
+public class SpringApplicationContext implements ApplicationContextInitializer {
 
     private static ApplicationContext context;
-
-    @Override
-    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        context = applicationContext;
-    }
 
     public static Object getObject (String name){
         return context.getBean(name);
@@ -24,5 +18,10 @@ public class SpringApplicationContext implements ApplicationContextAware{
 
     public static <T> T getObjct(Class<T> clz,String name){
         return context.getBean(name,clz);
+    }
+
+    @Override
+    public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
+        context = configurableApplicationContext;
     }
 }
